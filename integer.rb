cafe155b1aebfbc259e748b6c6975a2605490be7
Raphaël Bakhouche <raphael.bakhouche@epitech.eu>
2025-10-14 20:04:23 +0200

    init

---
+++ integer.rb
@@ -0,0 +1,15 @@
+puts "Création d'un entier"
+puts Integer(42)
+puts Integer("42")
+puts Integer(42.42)
+
+puts "\nUtilisation de la séparation des chiffres"
+puts 123456789
+puts 123_456_789
+
+puts "\nUtilisation des notations"
+puts 10
+puts 0b10
+puts 0o10
+puts 0x10
+puts 0xFF
